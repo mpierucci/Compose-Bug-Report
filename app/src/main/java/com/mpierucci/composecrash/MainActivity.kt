6dd47e93eacb61d8c3bfdfbe2c0e11256a40624a
marcopierucci <marco1pierucci@gmail.com>
2022-09-04 01:27:16 +0200

    Create minimal repro setup (Remove ViewModel)

--- app/src/main/java/com/mpierucci/composecrash/MainActivity.kt
+++ app/src/main/java/com/mpierucci/composecrash/MainActivity.kt
@@ -2,28 +2,25 @@ package com.mpierucci.composecrash
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
 import com.mpierucci.composecrash.ui.theme.ComposeCrashTheme
 
 class MainActivity : AppCompatActivity() {
-
-    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,52 +28,37 @@ class MainActivity : AppCompatActivity() {
 
         setContent {
             ComposeCrashTheme {
-                val state by viewModel.state.observeAsState()
-
                 LazyRow {
                     item {
-                        Render(state!!)
+                        Render()
                     }
                 }
             }
         }
     }
 
-
     @Composable
-    fun Render(state: State) {
-        when(state.pageOne) {
+    fun Render() {
+        val (mutate, onMutate) = remember { mutableStateOf(false) }
+        when (mutate) {
             false -> {
-                Column(modifier = Modifier
-                    .fillMaxSize()
-                    .verticalScroll(rememberScrollState())) {
-
-                    PageOneContent()
-                    PageOneButtons(viewModel::updatePageOne)
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .systemBarsPadding()
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    OutlinedTextField(value = "Lorem", onValueChange = {})
+                    Button(onClick = { onMutate(true) }) {
+                        Text(text = "Mutate State")
+                    }
                 }
             }
-            else -> {
+            true -> {
                 Box(modifier = Modifier.fillMaxSize()) {
 
                 }
             }
-        }
-    }
-
-
-    @Composable
-    private fun PageOneContent() {
-        Column {
-            Text(text = "Lorem Impsum", color = MaterialTheme.colors.onBackground)
-            OutlinedTextField(value = "Lorem", onValueChange = {})
-        }
-    }
-
-
-    @Composable
-    private fun PageOneButtons(onUpdateClick: () -> Unit) {
-        Button(onClick = onUpdateClick) {
-            Text(text = "Mutate PageOne State")
         }
     }
 }
